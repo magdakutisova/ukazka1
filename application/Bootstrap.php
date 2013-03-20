7a04f20ba13de9bcae8f840bc18aa2275f7743bc
magdakutisova <kutisovamagda@gmail.com>
2013-03-20 15:05:55 +0100

    zmena rout

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -40,48 +40,48 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 		$router = $frontController->getRouter();
 		
 		$router->addRoute('bookIndex',
-				new Zend_Controller_Router_Route('knihy',
+				new Zend_Controller_Router_Route('book',
 						array('controller' => 'book',
 								'action' => 'index'))
 				);
 		
 		$router->addRoute('bookDetail',
-				new Zend_Controller_Router_Route('kniha/:idBook/detail',
+				new Zend_Controller_Router_Route('book/detail/:idBook',
 						array('controller' => 'book',
 								'action' => 'detail'))
 				);
 		
 		$router->addRoute('bookNew',
-				new Zend_Controller_Router_Route('kniha/nova',
+				new Zend_Controller_Router_Route('book/new',
 						array('controller' => 'book',
 								'action' => 'new'))
 				);
 		
 		$router->addRoute('bookEdit',
-				new Zend_Controller_Router_Route('kniha/:idBook/upravit',
+				new Zend_Controller_Router_Route('book/edit/:idBook',
 						array('controller' => 'book',
 								'action' => 'edit'))
 		);
 		
 		$router->addRoute('bookDelete',
-				new Zend_Controller_Router_Route('kniha/:idBook/smazat',
+				new Zend_Controller_Router_Route('book/delete/:idbook',
 						array('controller' => 'book',
 								'action' => 'delete'))
 		);
 		
 		$router->addRoute('userRegister',
-				new Zend_Controller_Router_Route('registrace',
+				new Zend_Controller_Router_Route('user/register',
 						array('controller' => 'user',
 								'action' => 'register'))
 		);
 		
 		$router->addRoute('userLogin',
-				new Zend_Controller_Router_Route('prihlaseni',
+				new Zend_Controller_Router_Route('user/login',
 						array('controller' => 'user',
 								'action' => 'login')));
 		
 		$router->addRoute('userLogout',
-				new Zend_Controller_Router_Route('odhlaseni',
+				new Zend_Controller_Router_Route('user/logout',
 						array('controller' => 'user',
 								'action' => 'logout')));
 	}
