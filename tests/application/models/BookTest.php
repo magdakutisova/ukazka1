4710e51e7c18dc8f955c2088e52b49523914be68
magdakutisova <kutisovamagda@gmail.com>
2013-04-10 19:36:36 +0200

    testy

--- tests/application/models/BookTest.php
+++ tests/application/models/BookTest.php
@@ -1,67 +1,90 @@
 <?php
-class BookTest extends Zend_Test_PHPUnit_DatabaseTestCase{
-	
-	private $_connectionMock;
-	
-	//NEFUNKČNÍ NA PHPUNIT 3.6, NUTNO POUŽÍT NIŽŠÍ VERZE
-	//http://framework.zend.com/issues/browse/ZF-11781
+class BookTest extends PHPUnit_Framework_Testcase{
 	
 	public function setUp(){
 		$this->bootstrap = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
 		parent::setUp();
 	}
 	
-	protected function getConnection(){
-		if($this->_connectionMock == null){
-			$connection = Zend_Db::factory(new Zend_Db_Adapter_Pdo_Mysql(array(
-					'host' => '127.0.0.1',
-					'username' => 'root',
-					'password' => '',
-					'dbname' => 'ukazkaTest',
-					)));
-			$this->_connectionMock = $this->createZendDbConnection($connection, 'book');
-			Zend_Db_Table_Abstract::setDefaultAdapter($connection);
-		}
-		return $this->_connectionMock;
+	public function testBookInitiaState(){
+		$book = new Application_Model_Book();
+		
+		$this->assertNull($book->idBook, 'proměnná "idBook" musí být zpočátku prázdná');
+		$this->assertNull($book->name, 'proměnná "name" musí být zpočátku prázdná');
+		$this->assertNull($book->author, 'proměnná "author" musí být zpočátku prázdná');
+		$this->assertNull($book->description, 'proměnná "description" musí být zpočátku prázdná');
+		$this->assertNull($book->price, 'proměnná "price" musí být zpočátku prázdná');
+		$this->assertNull($book->stock, 'proměnná "stock" musí být zpočátku prázdná');
+		$this->assertNull($book->image, 'proměnná "image" musí být zpočátku prázdná');
 	}
 	
-	protected function getDataSet(){
-		return $this->createFlatXmlDataSet(
-				dirname(__FILE__) . '/_files/bookSeed.xml'
-				);
-	}
-	
-	public function testBookInsertedIntoDatabase(){
-		$bookTable = new Application_Model_BookMapper();
+	public function testExchangeArraySetsPropertiesCorrectly(){
+		$book = new Application_Model_Book();
 		$data = array(
-				'name' => 'Pražská zima',
-				'author' => 'Madelaine Albrightová',
-				'price' => 358,
-				'stock' => 54,
-				);
-		$book = new Application_Model_Book($data);
-		$bookTable->save($book);
-		
-		$ds = new Zend_Test_PHPUnit_Db_DataSet_QueryDataSet($this->getConnection);
-		$ds->addTable('book', 'SELECT * FROM book');
-		
-		$this->assertDataSetsEqual(
-				$this->createFlatXmlDataSet(dirname(__FILE__) . "/_files/bookInsertIntoAssertion.xml"),
-				$ds
-				);
+				'idBook' => 1,
+				'name' => 'test',
+				'author' => 'test',
+				'description' => 'test',
+				'price' => 100,
+				'stock' => 100,
+				'image' => 'test.jpg',
+		);
+	
+		$book->exchangeArray($data);
+	
+		$this->assertSame($data['idBook'], $book->idBook, 'proměnná "idBook" nebyla správně nastavena');
+		$this->assertSame($data['name'], $book->name, 'proměnná "name" nebyla správně nastavena');
+		$this->assertSame($data['author'], $book->author, 'proměnná "author" nebyla správně nastavena');
+		$this->assertSame($data['description'], $book->description, 'proměnná "description" nebyla správně nastavena');
+		$this->assertSame($data['price'], $book->price, 'proměnná "price" nebyla správně nastavena');
+		$this->assertSame($data['stock'], $book->stock, 'proměnná "stock" nebyla správně nastavena');
+		$this->assertSame($data['image'], $book->image, 'proměnná "image" nebyla správně nastavena');
 	}
 	
-	public function testBookDelete(){
-		$bookTable = new Application_Model_BookMapper();
-		$bookTable->delete(3);
-		
-		$ds = new Zend_Test_PHPUnit_Db_DataSet_DbTableDataSet();
-		$ds->addTable($bookTable);
-		
-		$this->assertDataSetsEqual(
-				$this->createFlatXmlDataSet(dirname(__FILE__) . "/_files/bookDeleteAssertion.xml"),
-				$ds
-				);
+	public function testExchangeArraySetsPropertiesToNullIfKeysAreNotPresent(){
+		$book = new Application_Model_Book();
+		$book->exchangeArray(array(
+				'idBook' => 1,
+				'name' => 'test',
+				'author' => 'test',
+				'description' => 'test',
+				'price' => 100,
+				'stock' => 100,
+				'image' => 'test.jpg',
+		));
+		$book->exchangeArray(array());
+	
+		$this->assertNull($book->idBook, 'proměnná "idBook" má být null');
+		$this->assertNull($book->name, 'proměnná "name" má být null');
+		$this->assertNull($book->author, 'proměnná "author" má být null');
+		$this->assertNull($book->description, 'proměnná "description" má být null');
+		$this->assertNull($book->price, 'proměnná "price" má být null');
+		$this->assertNull($book->stock, 'proměnná "stock" má být null');
+		$this->assertNull($book->image, 'proměnná "image" má být null');
+	}
+	
+	public function testGetArrayCopyReturnsAnArrayWithPropertyValues(){
+		$book = new Application_Model_Book();
+		$data = array(
+				'idBook' => 1,
+				'name' => 'test',
+				'author' => 'test',
+				'description' => 'test',
+				'price' => 100,
+				'stock' => 100,
+				'image' => 'test.jpg',
+		);
+	
+		$book->exchangeArray($data);
+		$copyArray = $book->toArray();
+	
+		$this->assertSame($data['idBook'], $copyArray['idBook'], 'proměnná "idBook" nebyla správně nastavena');
+		$this->assertSame($data['name'], $copyArray['name'], 'proměnná "name" nebyla správně nastavena');
+		$this->assertSame($data['author'], $copyArray['author'], 'proměnná "author" nebyla správně nastavena');
+		$this->assertSame($data['description'], $copyArray['description'], 'proměnná "description" nebyla správně nastavena');
+		$this->assertSame($data['price'], $copyArray['price'], 'proměnná "price" nebyla správně nastavena');
+		$this->assertSame($data['stock'], $copyArray['stock'], 'proměnná "stock" nebyla správně nastavena');
+		$this->assertSame($data['image'], $copyArray['image'], 'proměnná "image" nebyla správně nastavena');
 	}
 	
 }
